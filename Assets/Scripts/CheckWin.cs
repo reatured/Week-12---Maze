7c2a8bc7d76467c3730e6eeef87716a8117b6411
reatured <61943376+reatured@users.noreply.github.com>
2021-07-08 16:05:38 -0400

    3. Win Stats

---
+++ Assets/Scripts/CheckWin.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckWin : MonoBehaviour
+{
+    public GameObject winMessage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        winMessage.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+
+            winMessage.SetActive(true);
+
+        }
+    }
+}
